c2d3b5f02b2ee7614d8529b9865fb8e6e5a88850
Platt <lplatt@usgs.gov>
2020-03-13 09:04:43 -0500

    delete excess fxns

--- 6_visualize/src/visualize_date_task_plan.R
+++ 6_visualize/src/visualize_date_task_plan.R
@@ -45,26 +45,3 @@ create_visualize_date_tasks <- function(date_range, log_folder){
     final_steps='make_map',
     ind_dir='6_visualize/log')
 }
-
-
-# helper function to sprintf a bunch of key-value (string-variableVector) pairs,
-# then paste them together with a good separator for constructing remake recipes
-psprintf <- function(..., sep='\n      ') {
-  args <- list(...)
-  non_null_args <- which(!sapply(args, is.null))
-  args <- args[non_null_args]
-  argnames <- sapply(seq_along(args), function(i) {
-    nm <- names(args[i])
-    if(!is.null(nm) && nm!='') return(nm)
-    val_nm <- names(args[[i]])
-    if(!is.null(val_nm) && val_nm!='') return(val_nm)
-    return('')
-  })
-  names(args) <- argnames
-  strs <- mapply(function(template, variables) {
-    spargs <- if(template == '') list(variables) else c(list(template), as.list(variables))
-    do.call(sprintf, spargs)
-  }, template=names(args), variables=args)
-  paste(strs, collapse=sep)
-}
-
